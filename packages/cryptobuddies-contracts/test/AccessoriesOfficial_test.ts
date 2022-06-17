4d0f97ae1932f10433f1e802dc8cc3656ff9f489
Maksym Naboka <maksym.naboka@gmail.com>
2022-06-17 11:40:44 -0700

    Add tests

--- packages/cryptobuddies-contracts/test/AccessoriesOfficial_test.ts
+++ packages/cryptobuddies-contracts/test/AccessoriesOfficial_test.ts
@@ -1,32 +1,32 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { Contract, ContractFactory } from "ethers";
+import { ContractFactory } from "ethers";
 import { ethers } from "hardhat";
-import { CryptoBuddiesAccessoriesOfficial } from "../typechain-types";
+import { AccessoriesOfficial } from "../typechain-types";
 
-describe("CryptoBuddiesAccessoriesOfficial", function () {
+describe("AccessoriesOfficial", function () {
   let owner: SignerWithAddress,
     addr1: SignerWithAddress,
     addr2: SignerWithAddress;
 
-  let CryptoBuddiesAccessoriesOfficialFactory: ContractFactory;
-  let cbao: CryptoBuddiesAccessoriesOfficial;
+  let AccessoriesOfficialFactory: ContractFactory;
+  let cbao: AccessoriesOfficial;
 
   beforeEach(async () => {
     [owner, addr1, addr2] = await ethers.getSigners();
 
-    CryptoBuddiesAccessoriesOfficialFactory = await ethers.getContractFactory(
-      "CryptoBuddiesAccessoriesOfficial"
+    AccessoriesOfficialFactory = await ethers.getContractFactory(
+      "AccessoriesOfficial"
     );
 
-    cbao = (await CryptoBuddiesAccessoriesOfficialFactory.deploy(
+    cbao = (await AccessoriesOfficialFactory.deploy(
       "https://example.com/metadata/{id}.json"
-    )) as CryptoBuddiesAccessoriesOfficial;
+    )) as AccessoriesOfficial;
 
     await cbao.deployed();
   });
 
-  it("Should deploy CryptoBuddiesAccessoriesOfficial with uri from constructor", async function () {
+  it("Should deploy AccessoriesOfficial with uri from constructor", async function () {
     expect(await cbao.name()).to.equal("Crypto Buddies Accessories Official");
     expect(await cbao.symbol()).to.equal("CBAO1");
     expect(await cbao.uri(0)).to.equal(
@@ -102,5 +102,30 @@ describe("CryptoBuddiesAccessoriesOfficial", function () {
 
     // token id 1 should point to a new URI
     expect(await cbao.uri(1)).to.equal("aabbcc");
+  });
+
+  it("Should deploy without default uri and add individual uri for tokens", async function () {
+    const AccessoriesOfficialFactory2 = await ethers.getContractFactory(
+      "AccessoriesOfficial"
+    );
+
+    const cbao2 = (await AccessoriesOfficialFactory2.deploy(
+      ""
+    )) as AccessoriesOfficial;
+
+    await cbao2.deployed();
+
+    // tokens ids 0, 1 should have a different URI
+    expect(await cbao2.uri(0)).to.equal("");
+    expect(await cbao2.uri(1)).to.equal("");
+
+    // mint a token
+    await cbao2.mint(owner.address, 0, 1, []);
+
+    await cbao2.setUri(0, "https://uri");
+
+    // tokens ids 0, 1 should have a different URI
+    expect(await cbao2.uri(0)).to.equal("https://uri");
+    expect(await cbao2.uri(1)).to.equal("");
   });
 });
